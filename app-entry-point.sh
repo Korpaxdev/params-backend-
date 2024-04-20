1233ea7968cc7ec290081f525ac3c248ca82dcc2
korpaxdev <korpaxdev@gmail.com>
2024-04-20 16:05:21 +0300

    add apply fixtures in app-entry-point.sh

--- app-entry-point.sh
+++ app-entry-point.sh
@@ -2,6 +2,9 @@
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
+# Apply backend fixtures
+echo "Apply fixtures"
+python manage.py loaddata backend/fixtures/params_fixture.json
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
